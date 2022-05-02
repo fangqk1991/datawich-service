b6da9935c478bf075dae431c8619f36ad412c901
fangqk1991 <me@fangqk.com>
2022-05-02 14:33:32 +0800

    (models): +datahub models

--- src/models/datahub-sync/_DatahubColumn.ts
+++ src/models/datahub-sync/_DatahubColumn.ts
@@ -1,7 +1,29 @@
 import __DatahubColumn from '../auto-build/__DatahubColumn'
+import { _DatahubTable } from './_DatahubTable'
 
 export class _DatahubColumn extends __DatahubColumn {
   public constructor() {
     super()
+  }
+
+  public getColumnDatabaseSpec() {
+    const match = this.columnType.match(/^varchar\((\d+)\)$/)
+    const columnType = match && Number(match[1]) >= 1024 ? 'TEXT' : this.columnType
+    // return `${columnType} ${this.nullable ? 'NULL' : 'NOT NULL'} ${this.extrasInfo}`
+    // return `${columnType} ${this.nullable ? 'NULL' : 'NOT NULL'}`
+    return `${columnType} NULL`
+  }
+
+  public async updateName(name: string) {
+    this.fc_edit()
+    this.name = name
+    await this.updateToDB()
+  }
+
+  public sqlTableName() {
+    const table = new _DatahubTable()
+    table.engineKey = this.engineKey
+    table.tableKey = this.tableKey
+    return table.sqlTableName()
   }
 }
