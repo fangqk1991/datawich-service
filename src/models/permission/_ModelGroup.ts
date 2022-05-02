628f874fb6f0dbdf14a2a3265fefe0fd56f49660
fangqk1991 <me@fangqk.com>
2022-05-02 14:03:49 +0800

    (models): +permission models

--- src/models/permission/_ModelGroup.ts
+++ src/models/permission/_ModelGroup.ts
@@ -4,4 +4,10 @@ export class _ModelGroup extends __ModelGroup {
   public constructor() {
     super()
   }
+
+  public static async itemsForModelKey(modelKey: string) {
+    const searcher = new _ModelGroup().fc_searcher()
+    searcher.processor().addConditionKV('model_key', modelKey)
+    return searcher.queryFeeds()
+  }
 }
