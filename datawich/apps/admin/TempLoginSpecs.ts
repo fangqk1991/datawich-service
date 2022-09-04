764aa72f70665045442a977a5749c7ee367f975e
Fang <me@fangqk.com>
2022-09-04 15:50:50 +0800

    (package.json): upgrade backend-kit

--- datawich/apps/admin/TempLoginSpecs.ts
+++ datawich/apps/admin/TempLoginSpecs.ts
@@ -1,13 +1,11 @@
-import assert from '@fangcha/assert'
 import * as jsonwebtoken from 'jsonwebtoken'
 import { SpecFactory, SwaggerDocItem } from '@fangcha/router'
-import { OAuthClient } from '@fangcha/tools/lib/oauth-client'
 import { _SessionApp, FangchaSession } from '@fangcha/router/lib/session'
-import { KitAdminSsoApis } from '@fangcha/backend-kit/lib/apis'
+import { KitSsoApis } from '@fangcha/backend-kit/lib/apis'
 
 const factory = new SpecFactory('SSO', { skipAuth: true })
 
-factory.prepare(KitAdminSsoApis.Login, async (ctx) => {
+factory.prepare(KitSsoApis.Login, async (ctx) => {
   const aliveSeconds = 24 * 3600
   const session = ctx.session as FangchaSession
   const jwt = jsonwebtoken.sign({ email: 'user@example.com' }, _SessionApp.jwtProtocol.jwtSecret, {
@@ -17,7 +15,7 @@ factory.prepare(KitAdminSsoApis.Login, async (ctx) => {
   ctx.redirect(session.getRefererUrl())
 })
 
-factory.prepare(KitAdminSsoApis.Logout, async (ctx) => {
+factory.prepare(KitSsoApis.Logout, async (ctx) => {
   ctx.cookies.set(_SessionApp.jwtProtocol.jwtKey, '', {
     maxAge: 0,
   })
