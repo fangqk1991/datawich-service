57112114354dd391acecf950cf2d12324302ce7c
Fang <me@fangqk.com>
2022-10-20 19:37:45 +0800

    v0.22.2: change EmailEntityModel

--- src/common/models/EmailEntityModel.ts
+++ src/common/models/EmailEntityModel.ts
@@ -1,13 +1,6 @@
-interface SimpleUser {
-  iamId: number
-  name: string
-  email: string
-}
-
 export interface EmailEntityModel {
   title: string
   contentTemplate: string
-  // TODO: 统一 receiverList、ccReceiverList 类型为 string[]
-  receiverList: SimpleUser[]
+  receiverList: string[]
   ccReceiverList: string[]
 }
